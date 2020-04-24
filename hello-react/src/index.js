b118caccac07300f66020c39ca50e8611c6ae71a
tangbin <tangbin@uqpay.com>
2020-04-24 16:58:58 +0800

    feat: 事件监听

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -3,9 +3,27 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 class Title extends Component {
+    handleClickOnTitle() {
+        console.log('Click on title.')
+    }
+
+    handleClickOnTitleWithEvent(e) {
+        console.log(e.target.innerHTML)
+    }
+
+    // In React, how to call multiple functions with their own arguments?
+    // https://stackoverflow.com/questions/51435345/in-react-how-to-call-multiple-functions-with-their-own-arguments
+    handleClickOnTitleWithBindThis(e) {
+        console.log(this)
+    }
+
     render() {
         return (
-            <h1>React 小书</h1>
+            <h1 onClick={(event) => {
+                this.handleClickOnTitle()
+                this.handleClickOnTitleWithEvent(event)
+                this.handleClickOnTitleWithBindThis.call(this, event)
+            }}>React 小书</h1>
         )
     }
 
@@ -55,6 +73,6 @@ class Index extends Component {
 }
 
 ReactDOM.render(
-    <Index />,
+    <Index/>,
     document.getElementById('root')
 )
