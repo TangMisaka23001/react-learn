322d1d67b5b610802730dc52488ac99e330919a1
tangbin <tangbin@uqpay.com>
2020-04-24 16:34:39 +0800

    feat: 组件的组合、嵌套和组件树

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,44 +1,60 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+class Title extends Component {
+    render() {
+        return (
+            <h1>React 小书</h1>
+        )
+    }
+
+}
+
 class Header extends Component {
-  render () {
-    return (
-        <div>
-          <h1>React 小书</h1>
-        </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Title/>
+                <h2>This is Header</h2>
+            </div>
+        )
+    }
+}
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This is main content</h2>
+            </div>
+        )
+    }
+}
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This is footer</h2>
+            </div>
+        )
+    }
+}
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Main/>
+                <Footer/>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
-    <Header />,
+    <Index />,
     document.getElementById('root')
 )
-
-// 编译以后会变成
-// import React, { Component } from 'react'
-// import ReactDOM from 'react-dom'
-// import './index.css'
-//
-// class Header extends Component {
-//     render () {
-//         return (
-//             React.createElement(
-//                 "div",
-//                 null,
-//                 React.createElement(
-//                     "h1",
-//                     { className: 'title' },
-//                     "React 小书"
-//                 )
-//             )
-//         )
-//     }
-// }
-//
-// ReactDOM.render(
-//     React.createElement(Header, null),
-//     document.getElementById('root')
-// );
