cb1b21f11ac4ca522425f3a333040a5fcb99e0f0
tangbin <tangbin@uqpay.com>
2020-04-26 13:50:09 +0800

    feat: add comment created time

--- comment-app/src/Comment.js
+++ comment-app/src/Comment.js
@@ -2,6 +2,22 @@ import React, {Component} from 'react'
 
 class Comment extends Component {
 
+    componentWillMount () {
+        this._updateTimeString()
+        this._timer = setInterval(
+            this._updateTimeString.bind(this),
+            5000
+        )
+    }
+    _updateTimeString () {
+        const comment = this.props.comment
+        const duration = (+Date.now() - comment.createdTime) / 1000
+        this.setState({
+            timeString: duration > 60
+                ? `${Math.round(duration / 60)} 分钟前`
+                : `${Math.round(Math.max(duration, 1))} 秒前`
+        })
+    }
     render() {
         return (
             <div className={'comment'}>
@@ -9,6 +25,9 @@ class Comment extends Component {
                     <span>{this.props.comment.username} </span>：
                 </div>
                 <p>{this.props.comment.content}</p>
+                <span className={'comment-createdtime'}>
+                    {this.state.timeString}
+                </span>
             </div>
         )
     }
