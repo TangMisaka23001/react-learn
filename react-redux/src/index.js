6fbc10a5603e2317fc45e20d0106f97d1b1e7a0c
tangbin <tangbin@uqpay.com>
2020-04-26 15:13:39 +0800

    refactor: update index.js with Provider

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 import Header from './Header'
 import Content from './Content'
 import './index.css'
+import {Provider} from './connect'
 
 function createStore (reducer) {
     let state = null
@@ -32,26 +33,21 @@ const themeReducer = (state, action) => {
 const store = createStore(themeReducer)
 
 class Index extends Component {
-    static childContextTypes = {
-        store: PropTypes.object
-    }
 
-    getChildContext () {
-        return { store }
-    }
-
-    render () {
+    render() {
         return (
             <div>
-                <Header />
-                <Content />
+                <Header/>
+                <Content/>
             </div>
         )
     }
 }
 
+// 把 Provider 作为组件树的根节点
 ReactDOM.render(
-    <Index />,
+    <Provider store={store}>
+        <Index/>
+    </Provider>,
     document.getElementById('root')
 )
-
